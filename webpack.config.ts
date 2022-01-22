6d270fe615ecb3744021c68be62d894f0d169120
Jake Crane <jake@jakecrane.com>
2022-01-22 14:42:51 -0600

    Move babel config to its own file

--- webpack.config.ts
+++ webpack.config.ts
@@ -20,18 +20,6 @@ const config: Configuration = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            [
-                                "@babel/preset-react",
-                                {
-                                    "runtime": "automatic"
-                                }
-                            ],
-                            "@babel/preset-typescript",
-                        ],
-                    },
                 },
             },
         ],
