12b79f4fb78338819eb447bf6d04b757d9640171
Jake Crane <jake@jakecrane.com>
2022-01-22 13:55:32 -0600

    Added a proper build

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 This is an example of using react-intl to translate text based on a selected locale.
 
-Still need to work on getting it to work with a prod build
-
 One issue it still has is that it allows unused messages to end up in the resulting translation files.
--- webpack.config.ts
+++ webpack.config.ts
@@ -4,13 +4,15 @@ import { Configuration as WebpackConfiguration } from "webpack";
 import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 import express from "express";
 
+const isProduction = process.argv[process.argv.indexOf('--mode') + 1] === 'production';
+
 interface Configuration extends WebpackConfiguration {
     devServer?: WebpackDevServerConfiguration;
 }
 
 const config: Configuration = {
     entry: "./src/index.tsx",
-    devtool: 'eval-source-map',
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
     module: {
         rules: [
             {
